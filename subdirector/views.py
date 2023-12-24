41fe639d4722dda8ad72e7cd0b0f443b94a0d9a9
Alvaro Arenas <alvaro.arenas973@gmail.com>
2023-12-24 20:31:27 -0300

    ADD: Templates, urls y views base en subdirector

--- subdirector/views.py
+++ subdirector/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def inicio(request):
+    try:
+        username = request.session['username']
+        return redirect('subdirector/perfil')
+    except KeyError:
+        return redirect('/login')
+
+def perfil(request):
+    try:
+        username = request.session['username']
+        return render('subdirector/perfil')
+    except KeyError:
+        return redirect('/login')
